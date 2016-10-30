b2042c811ea6f6bd43157fbecd84374f598948e8
Brian Veltman <b.veltman@lupio.nl>
2016-10-30 22:08:58 +0100

    Add dismissable message component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
 import Wrapper from './Components/Common/Wrapper';
+import DismissMessage from './Components/Common/Messages/Message';
 
 
 class App extends Component {
   render() {
     return (
       <Container>
+      <DismissMessage onDismiss={this.props.handleDismiss} info />
       <Wrapper />
       </Container>
     );
